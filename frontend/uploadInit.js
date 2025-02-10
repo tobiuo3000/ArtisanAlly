940483b98cabb4b245d89d6f1f1c3bd964ace342
kunaisn <naokisawahata11@yahoo.co.jp>
2025-02-10 16:38:35 +0900

    fix

--- frontend/uploadInit.js
+++ frontend/uploadInit.js
@@ -68,9 +68,9 @@ async function handleImageUpload(file) {
   try {
     const jsonData = await getJsonData(file);
     // ここを消してした二つのコメントアウトを戻す
-    const docId = "593ea0ec-47ed-4ff1-918a-7cf2512d9d92";
-    // const docId = await sendImageToApi(jsonData);
-    // await callAPIs(docId);
+    // const docId = "593ea0ec-47ed-4ff1-918a-7cf2512d9d92";
+    const docId = await sendImageToApi(jsonData);
+    await callAPIs(docId);
     docRefId = docId;
     const firestoreDoc = await getFirestoreDoc(docId);
     displayImageData(firestoreDoc);
